3ef03880a94d4bf665dccb0557c5a8d0198b9e81
ritishka11 <154485343+ritishka11@users.noreply.github.com>
2025-04-19 16:18:29 +0530

    Update Mycalculator2.py
    This Python program implements a basic calculator using Object-Oriented Programming (OOP) principles. The calculator allows users to input two numbers and choose an operation (addition, subtraction, multiplication, division, power, modulus) via a menu interface. It performs the selected operation and returns the result. The program uses a Calculator class with methods for each operation, encapsulating the logic in a structured and reusable way.

--- Mycalculator2.py
+++ Mycalculator2.py
@@ -1,8 +1,11 @@
+# Define a Calculator class to encapsulate the operations
 class Calculator:
+# Constructor to initialize the two numbers
     def __init__(self, a,b):
         self.a = a
         self.b = b
-
+        
+# Methods to perform operation
     def add(self):
         return self.a+self.b
     def subtract(self):
@@ -15,13 +18,16 @@ class Calculator:
         return self.a**self.b
     def modulus(self):
         return self.a%self.b
-    
+        
+# Get user input for the two numbers    
 fnumber1 = input("Enter the first number: ")
 fnumber2 = input("Enter the second number: ")
 
+# Convert input strings to integers
 a = int(fnumber1)
 b = int(fnumber2)
 
+# Display the menu of operations for the user to select
 print("Select an operation:")
 print("1. Addition")
 print("2. Subtraction")
@@ -30,20 +36,24 @@ print("4. Multiplication")
 print("5. Power")
 print("6. Modulus")
 
+# Get the operation choice from the user and convert to int as the input fun returns a string
 operation = input("Enter the number corresponding to the operation that you wish to perform:\n")
 operation = int(operation)
 
+# Create an instance of the Calculator class with the user-provided numbers
 calci = Calculator(a,b)
 
+# Perform the selected operation based on user's choice
 if operation == 1:
-    result = calci.add()
+    result = calci.add()  # Call the add method
 elif operation == 2:
-    result = calci.subtract()
+    result = calci.subtract() # Call the subtract method
 elif operation == 3:
+# Check for division by zero
     if b == 0:
-        result = "Please enter a digit > 0"
+        result = "Please enter a digit > 0" # Handle division by zero error
     else:
-        result = calci.divide()
+        result = calci.divide()  # Call the divide method
 elif operation == 4:
     result = calci.multiplication()
 elif operation == 5:
@@ -51,6 +61,6 @@ elif operation == 5:
 elif operation == 6:
     result = calci.modulus()
 else:
-    result = "Invalid Input"
+    result = "Invalid Input" # Handle invalid operation inpu
 
 print("Result is:", result)
