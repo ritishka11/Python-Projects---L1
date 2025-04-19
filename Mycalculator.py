d94800c01d373f571150c0e7bf33df9c247eac2d
ritishka11 <154485343+ritishka11@users.noreply.github.com>
2025-04-19 15:32:56 +0530

    Update Mycalculator.py
    The user is prompted to input two numbers.
    
    The menu provides the user with options for different operations.
    
    The user selects an operation by entering a number corresponding to their choice.
    
    The program performs the calculation based on the user's input and prints the result.

--- Mycalculator.py
+++ Mycalculator.py
@@ -1,13 +1,44 @@
+# Get user inputs for numbers
 fnumber1 = input("Enter first number: ")
 fnumber2 = input("Enter second number: ")
+
+# Converting the number inputted to integer as the "input func" always returns a string
 a = int(fnumber1)
 b = int(fnumber2)
-operation = input("Choose the operation you want to perform from the below: Enter + for addition \n Enter - for subtraction")
-if operation == "+":
+
+# Menu for selecting operation
+print("Select an operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Division")
+print("4. Multiplication")
+print("5. Power")
+print("6. Modulus")
+
+# Get user's operation choice
+operation = input("Enter the number corresponding to the operation that you wish to perform:\n")
+
+# Convert to int as input returns a string
+operation = int(operation)
+
+# Perform calculation as per the user's choice
+if operation == 1:
     result = a+b
-elif operation == "-":
+elif operation == 2:
     result = a-b
+elif operation == 3:
+    if b == 0:
+        result = "Please enter a digit > 0"
+    else:
+        result = a/b
+elif operation == 4:
+    result = a*b
+elif operation == 5:
+    result = a**b
+elif operation == 6:
+    result = a%b
 else:
     result = "Invalid Input"
-print("Result is: ", result)
 
+# Print output of the result
+print("Result is: ", result)
